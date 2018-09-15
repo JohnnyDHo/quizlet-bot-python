903cab85bfc832880769be64dd492583739c6a2c
nathan <njp3@rice.edu>
2018-09-15 15:48:07 -0500

    hi statement

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def receive_message():
                 elif "quiz" in message['message'].get('text').lower():
                     quiz1 = quiztext()
                     send_message(recipient_id, quiz1)
-                    if message['message'].get('text').lower() == quiztext():
+                    if quiztext() in message['message'].get('text').lower():
                         send_message(recipient_id, "correct")
 
     return "Message Processed"
