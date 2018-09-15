97927fcfb6d57f53791b8fb55a9d1ad4b8c52ad9
hs45 <hs45@rice.edu>
2018-09-15 12:46:03 -0500

    0.0

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def verify_fb_token(token_sent):
 
 # Chooses a message to send to the user
 def get_message_text(message):
-    return "Hi HackRice TeamMMMMMMMMMM"
+    return "function working"
 
 ## Send text message to recipient
 def send_message(recipient_id, response):
