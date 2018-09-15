ae2dc257c6e3724fbce3616a3f63cf1142f3000a
nathan <njp3@rice.edu>
2018-09-15 17:24:49 -0500

    hi statement

--- app.py
+++ app.py
@@ -1,64 +1,117 @@
-## Python libraries that we need to import for our bot
+# Python libraries that we need to import for our bot
 from flask import Flask, request
 from pymessenger.bot import Bot ## pymessenger is a Python wrapper for the Facebook Messenger API
 import os
 
-app = Flask(__name__) ## This is how we create an instance of the Flask class for our app
+app = Flask(__name__) # This is how we create an instance of the Flask class for our app
 
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-VERIFY_TOKEN = os.environ['VERIFY_TOKEN'] ## Replace 'VERIFY_TOKEN' with your verify token
-bot = Bot(ACCESS_TOKEN) ## Create an instance of the bot
+VERIFY_TOKEN = os.environ['VERIFY_TOKEN'] # Replace 'VERIFY_TOKEN' with your verify token
+bot = Bot(ACCESS_TOKEN) # Create an instance of the bot
+
+q1 = "Question 1: What animal has a long neck?"
+a1 = "Giraffe"
+q2 = "Question 2: What animal is man's best friend?"
+a2 = "Dog"
+q3 = "Question 3: What animal makes the noise 'meow'"
+a3 = "Cat"
+q4 = "Question 4: What animal has one horn"
+a4 = "Rhino"
+q5 = "Question 5: What is Rice's mascot?"
+a5 = "Owl"
+
+questions = [q1, q2, q3, q4, q5]
+answers = [a1, a2, a3, a4, a5]
+state = "None"
+correct_count = 0
+q_index = 0
 
 def verify_fb_token(token_sent):
-    ## Verifies that the token sent by Facebook matches the token sent locally
+    # Verifies that the token sent by Facebook matches the token sent locally
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
 # Chooses a message to send to the user
-import random
-def get_message_text():
-    return "Hi HackRice Team"
-def quiztext():
-    question = ["mans best friend", "says meow", "long neck"]
-    return random.choice(question)
+def correct_response():
+    global correct_count
+    correct_count += 1
+    return "Correct."
 
-## Send text message to recipient
+def incorrect_response():
+    return "Incorrect."
+
+def run_program(recipient_id, message):
+    global state, q_index, correct_count
+    if state == "None":
+        if message == "hello"
+            send_message(recipient_id, "Welcome to Quizlett-Bot. Send 'quiz' to get started)
+        if message == "start quiz":
+            state = "quiz"
+            send_message(recipient_id, questions[q_index])
+        else:
+            send_message(recipient_id, "Send \"start quiz\" to start quiz")
+    elif state == "quiz":
+        if "end quiz" in message:
+            state = "done quiz"
+            q_index = 0
+            send_message(recipient_id, "Your quiz has been terminated. Send \"Get Result\" to see your result.")
+        else:
+            if message == answers[q_index]:
+                send_message(recipient_id, correct_response())
+            else:
+                send_message(recipient_id, "You response is " + message + ". The correct answer is " + answers[q_index])
+
+            q_index += 1
+
+            if q_index > len(questions) - 1:
+                state = "done quiz"
+                q_index = 0
+                send_message(recipient_id, "You've reached the end of the quiz.")
+                send_message(recipient_id, "Send \"Get Result\" to see your result.")
+                state = "done quiz"
+            else:
+                send_message(recipient_id, questions[q_index])
+
+    elif state == "done quiz" and message == "get result":
+        send_message(recipient_id, "Your got " + str(correct_count) + "/" + str(len(questions)) + " correct.")
+
+
+# Send text message to recipient
 def send_message(recipient_id, response):
-    bot.send_text_message(recipient_id, response) ## Sends the 'response' parameter to the user
+    bot.send_text_message(recipient_id, response) # Sends the 'response' parameter to the user
     return "Message sent"
 
+def retrieve_id_and_message():
+    print("retrieve function gets called")
+    recipient_id = ""
+    message = ""
+    output = request.get_json()  # get whatever message a user sent the bot
+    for event in output['entry']:
+        messaging = event['messaging']
+        for message in messaging:
+            if message.get('message'):
+                recipient_id = message['sender']['id']
+                message = message['message'].get('text').lower()
+    return recipient_id, message
 
-## This endpoint will receive messages
+# This endpoint will receive messages
 @app.route("/webhook/", methods=['GET', 'POST'])
 def receive_message():
-    print("MESSAGE RECEIVED")
-    ## Handle GET requests
+    # Handle GET requests
     if request.method == 'GET':
-        token_sent = request.args.get("hub.verify_token") ## Facebook requires a verify token when receiving messages
+        print("GET request gets called")
+        token_sent = request.args.get("hub.verify_token")  # Facebook requires a verify token when receiving messages
         return verify_fb_token(token_sent)
 
-    ## Handle POST requests
+    # Handle POST requests
     else:
-       output = request.get_json() ## get whatever message a user sent the bot
-       for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
-            if message.get('message'):
-                recipient_id = message['sender']['id'] ## Facebook Messenger ID for user so we know where to send response back to
+        recipient_id, message = retrieve_id_and_message()
 
-                ## If user sends text
-                if "hackrice" in message['message'].get('text').lower():
-                    response_sent_text = get_message_text()
-                    send_message(recipient_id, response_sent_text)
-                elif "hi" in message['message'].get('text').lower():
-                    send_message(recipient_id, "Hi! Welcome to Quizlet-Bot! To pick a quiz send: 'quiz' To check history send: 'history' To upload your own quiz send: 'file'")
-                elif "quiz" in message['message'].get('text').lower():
-                    quiz1 = quiztext()
-                    send_message(recipient_id, quiz1)
+        run_program(recipient_id, message)
 
-    return "Message Processed"
+        return "Message Processed"
 
-## Ensures that the below code is only evaluated when the file is executed, and ignored if the file is imported
+# Ensures that the below code is only evaluated when the file is executed, and ignored if the file is imported
 if __name__ == "__main__":
     app.run() ## Runs application
