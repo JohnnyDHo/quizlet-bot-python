1960b13f35205fe27209e6b916b829653a1911bd
nathan <njp3@rice.edu>
2018-09-15 14:07:34 -0500

    hi statement

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def receive_message():
                 if "hackrice" in message['message'].get('text').lower():
                     response_sent_text = get_message_text()
                     send_message(recipient_id, response_sent_text)
-                elif "hi" or "hey" or "hello" in message['message'].get('text').lower():
+                elif "hi" in message['message'].get('text').lower():
                     send_message(recipient_id, "Hi! Welcome to Quizlet-Bot! To pick a quiz send: 'quiz' To check history send: 'history' To upload your own quiz send: 'file'")
                 elif "quiz" in message['message'].get('text').lower():
                         send_message(recipient_id, "nice dude")
