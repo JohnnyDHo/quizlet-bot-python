0a0a1f67de09a48525e4e3de9f352e4eb0935584
nathan <njp3@rice.edu>
2018-09-15 17:31:35 -0500

    hi statement

--- app.py
+++ app.py
@@ -9,28 +9,22 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN'] # Replace 'VERIFY_TOKEN' with your verify token
 bot = Bot(ACCESS_TOKEN) # Create an instance of the bot
 
-q1 = "Question 1: What animal has a long neck?"
-a1 = "Giraffe"
+q1 = "Question 1: What is Rice's mascot?"
+a1 = "Owl"
 q2 = "Question 2: What animal is man's best friend?"
 a2 = "Dog"
-q3 = "Question 3: What animal makes the noise 'meow'"
+q3 = "Question 3: What animal makes the sound 'meow'?"
 a3 = "Cat"
-q4 = "Question 4: What animal has one horn"
-a4 = "Rhino"
-q5 = "Question 5: What is Rice's mascot?"
-a5 = "Owl"
+q4 = "Question 4: What animal has a long neck?"
+a4 = "Giraffe"
+q5 = "Question 5: What animal has one horn?"
+a5 = "Rhino"
 
 questions = [q1, q2, q3, q4, q5]
 answers = [a1, a2, a3, a4, a5]
 state = "None"
 correct_count = 0
 q_index = 0
-
-def verify_fb_token(token_sent):
-    # Verifies that the token sent by Facebook matches the token sent locally
-    if token_sent == VERIFY_TOKEN:
-        return request.args.get("hub.challenge")
-    return 'Invalid verification token'
 
 # Chooses a message to send to the user
 def correct_response():
@@ -43,9 +37,10 @@ def incorrect_response():
 
 def run_program(recipient_id, message):
     global state, q_index, correct_count
+
     if state == "None":
-        if message == "hello"
-            send_message(recipient_id, "Welcome to Quizlett-Bot. Send 'quiz' to get started)
+        if message == "hello":
+            send_message(recipient_id, "Welcome to Quiz-Bot. To get started send 'quiz'")
         if message == "start quiz":
             state = "quiz"
             send_message(recipient_id, questions[q_index])
@@ -69,7 +64,6 @@ def run_program(recipient_id, message):
                 q_index = 0
                 send_message(recipient_id, "You've reached the end of the quiz.")
                 send_message(recipient_id, "Send \"Get Result\" to see your result.")
-                state = "done quiz"
             else:
                 send_message(recipient_id, questions[q_index])
 
@@ -77,7 +71,20 @@ def run_program(recipient_id, message):
         send_message(recipient_id, "Your got " + str(correct_count) + "/" + str(len(questions)) + " correct.")
 
 
-# Send text message to recipient
+
+
+
+
+
+
+# ======================== Don't mess with the stuff below!!! ========================
+
+def verify_fb_token(token_sent):
+    # Verifies that the token sent by Facebook matches the token sent locally
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
 def send_message(recipient_id, response):
     bot.send_text_message(recipient_id, response) # Sends the 'response' parameter to the user
     return "Message sent"
@@ -107,9 +114,7 @@ def receive_message():
     # Handle POST requests
     else:
         recipient_id, message = retrieve_id_and_message()
-
         run_program(recipient_id, message)
-
         return "Message Processed"
 
 # Ensures that the below code is only evaluated when the file is executed, and ignored if the file is imported
