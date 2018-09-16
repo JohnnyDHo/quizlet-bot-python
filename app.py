95344ba0f3becb4731fa9ac5c1863e8afa7632e8
Jeffrey Sheng <jeffrey_shengqifeng@yahoo.com>
2018-09-15 21:34:54 -0500

    rolled back to the working one

--- app.py
+++ app.py
@@ -2,16 +2,12 @@
 from flask import Flask, request
 from pymessenger.bot import Bot ## pymessenger is a Python wrapper for the Facebook Messenger API
 import os
-import time
 
 app = Flask(__name__) # This is how we create an instance of the Flask class for our app
 
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN'] # Replace 'VERIFY_TOKEN' with your verify token
 bot = Bot(ACCESS_TOKEN) # Create an instance of the bot
-
-# ======================== Don't mess with the stuff Above!!! ========================
-
 
 q1 = "Question 1: answer a1"
 a1 = "a1"
@@ -26,81 +22,9 @@ a5 = "a5"
 
 questions = [q1, q2, q3, q4, q5]
 answers = [a1, a2, a3, a4, a5]
-
-# state = "None"
-# correct_count = 0
-# q_index = 0
-
-users = {
-    # recipient-id : [state, correct_count, q_index]
-}
-
-# Chooses a message to send to the user
-def correct_response(recipient_id):
-    global users
-    users[recipient_id]["correct_count"] += 1
-    return "Correct."
-
-def run_program(recipient_id, message):
-    global users, questions, answers
-    id_not_found = False
-
-    if recipient_id not in users:
-        id_not_found = True
-        time.sleep(0.5)
-        send_message(recipient_id, "User ID " + str(recipient_id) + " not found")
-        users[recipient_id] = {}
-        time.sleep(0.5)
-        users[recipient_id]["state"] = "None"
-        time.sleep(0.5)
-        users[recipient_id]["correct_count"] = 0
-        time.sleep(0.5)
-        users[recipient_id]["q_index"] = 0
-        time.sleep(0.5)
-        send_message(recipient_id, "Hi, there. Your user ID is: " + str(recipient_id))
-        print(str(users))
-
-    if id_not_found == False:
-        send_message(recipient_id, "User ID " + str(recipient_id) + " is found")
-
-    if users[recipient_id]["state"] == "None":
-        send_message(recipient_id, "the state is " + users[recipient_id]["state"])
-        if message == "start quiz":
-            users[recipient_id]["state"] = "start quiz"
-            time.sleep(0.5)
-            print(str(users))
-            send_message(recipient_id, questions[users[recipient_id]["q_index"]])
-        else:
-            send_message(recipient_id, "Send \"start quiz\" to start quiz")
-
-    elif users[recipient_id]["state"] == "start quiz":
-        if "end quiz" in message:
-            users[recipient_id]["state"] = "done quiz"
-            users[recipient_id]["q_index"] = 0
-            send_message(recipient_id, "Your quiz has been terminated. Send \"Get Result\" to see your result.")
-        else:
-            if message == answers[users[recipient_id]["q_index"]]:
-                send_message(recipient_id, correct_response())
-            else:
-                send_message(recipient_id, "You response is " + message + ". The correct answer is " + answers[users[recipient_id]["q_index"]])
-
-            users[recipient_id]["q_index"] += 1
-
-            if users[recipient_id]["q_index"] > len(questions) - 1:
-                users[recipient_id]["state"] = "done quiz"
-                users[recipient_id]["q_index"] = 0
-                send_message(recipient_id, "You've reached the end of the quiz.")
-                send_message(recipient_id, "Send \"Get Result\" to see your result.")
-            else:
-                send_message(recipient_id, questions[users[recipient_id]["q_index"]])
-
-    elif users[recipient_id]["state"] == "done quiz" and message == "get result":
-        send_message(recipient_id, "Your got " + str(users[recipient_id]["correct_count"]) + "/" + str(len(questions)) + " correct.")
-
-
-
-
-# ======================== Don't mess with the stuff below!!! ========================
+state = "None"
+correct_count = 0
+q_index = 0
 
 def verify_fb_token(token_sent):
     # Verifies that the token sent by Facebook matches the token sent locally
@@ -108,6 +32,50 @@ def verify_fb_token(token_sent):
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
+# Chooses a message to send to the user
+def correct_response():
+    global correct_count
+    correct_count += 1
+    return "Correct."
+
+def incorrect_response():
+    return "Incorrect."
+
+def run_program(recipient_id, message):
+    global state, q_index, correct_count
+    if state == "None":
+        if message == "start quiz":
+            state = "quiz"
+            send_message(recipient_id, questions[q_index])
+        else:
+            send_message(recipient_id, "Send \"start quiz\" to start quiz")
+    elif state == "quiz":
+        if "end quiz" in message:
+            state = "done quiz"
+            q_index = 0
+            send_message(recipient_id, "Your quiz has been terminated. Send \"Get Result\" to see your result.")
+        else:
+            if message == answers[q_index]:
+                send_message(recipient_id, correct_response())
+            else:
+                send_message(recipient_id, "You response is " + message + ". The correct answer is " + answers[q_index])
+
+            q_index += 1
+
+            if q_index > len(questions) - 1:
+                state = "done quiz"
+                q_index = 0
+                send_message(recipient_id, "You've reached the end of the quiz.")
+                send_message(recipient_id, "Send \"Get Result\" to see your result.")
+                state = "done quiz"
+            else:
+                send_message(recipient_id, questions[q_index])
+
+    elif state == "done quiz" and message == "get result":
+        send_message(recipient_id, "Your got " + str(correct_count) + "/" + str(len(questions)) + " correct.")
+
+
+# Send text message to recipient
 def send_message(recipient_id, response):
     bot.send_text_message(recipient_id, response) # Sends the 'response' parameter to the user
     return "Message sent"
@@ -130,13 +98,16 @@ def retrieve_id_and_message():
 def receive_message():
     # Handle GET requests
     if request.method == 'GET':
+        print("GET request gets called")
         token_sent = request.args.get("hub.verify_token")  # Facebook requires a verify token when receiving messages
         return verify_fb_token(token_sent)
 
     # Handle POST requests
     else:
         recipient_id, message = retrieve_id_and_message()
+
         run_program(recipient_id, message)
+
         return "Message Processed"
 
 # Ensures that the below code is only evaluated when the file is executed, and ignored if the file is imported
