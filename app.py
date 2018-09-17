68512e9b743d4641b0f5f000d8db9f4ae24f0a7c
nathan <njp3@rice.edu>
2018-09-16 21:18:00 -0500

    hi statement

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-
 # Python libraries that we need to import for our bot
 from flask import Flask, request
-from pymessenger.bot import Bot ## pymessenger is a Python wrapper for the Facebook Messenger API
+from pymessenger.bot import Bot  ## pymessenger is a Python wrapper for the Facebook Messenger API
 import os
 import random
-app = Flask(__name__) # This is how we create an instance of the Flask class for our app
 
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-VERIFY_TOKEN = os.environ['VERIFY_TOKEN'] # Replace 'VERIFY_TOKEN' with your verify token
-bot = Bot(ACCESS_TOKEN) # Create an instance of the bot
+app = Flask(__name__)  # This is how we create an instance of the Flask class for our app
+
+ACCESS_TOKEN = 'EAADwbtv7Ug4BANxew62pyFzhzvHVNpiyWDNeXS5EBJBFKhlpEmtPOywwIhPUsCEDgE7UrroHezPZBcjzzjiptpZBbzy3iA3NvUuXZAiwglAwticZBu773BvSwHiVslyZCzM0h5WHs8wxQuRZCKuGMUHkRSQh6ZCcMOdTxg8vlmKVjyj033DZBbDU'
+VERIFY_TOKEN = 'TESTINGTOKEN'  # Replace 'VERIFY_TOKEN' with your verify token
+bot = Bot(ACCESS_TOKEN)  # Create an instance of the bot
+
+# ======================== Don't mess with the stuff Above!!! ========================
+
 
 q1 = "Question 1: What is Rice's mascot?"
 a1 = "owl"
@@ -23,61 +26,80 @@ a5 = "rhino"
 
 questions = [q1, q2, q3, q4, q5]
 answers = [a1, a2, a3, a4, a5]
-shuffle_index = range(len(questions))
-random.shuffle(shuffle_index)
-state = "None"
-correct_count = 0
-q_index = shuffle_index[0]
+#shuff_index = range(len(questions))
+#random.shuffle(shuff.index)
+
+# state = "None"
+# correct_count = 0
+# q_index = 0
+
+users = {
+    # recipient-id : [state, correct_count, q_index]
+}
+
 
 # Chooses a message to send to the user
-def correct_response():
-    global correct_count
-    correct_count += 1
+def correct_response(recipient_id):
+    global users
+    users[recipient_id]["correct_count"] += 1
     return "Correct."
 
-def incorrect_response():
-    return "Incorrect."
 
 def run_program(recipient_id, message):
-    global state, q_index, correct_count
+    global users, questions, answers
+    id_not_found = False
 
-    if state == "None":
+    if recipient_id not in users:
+        id_not_found = True
+        # send_message(recipient_id, "User ID " + str(recipient_id) + " not found")
+        users[recipient_id] = {}
+        users[recipient_id]["state"] = "None"
+        users[recipient_id]["correct_count"] = 0
+        users[recipient_id]["q_index"] = 0 #shuff_index[0]
+        send_message(recipient_id, "Welcome to QuizME! Your user ID is: " + str(recipient_id))
+        # print(str(users))
+
+    # if id_not_found == False:
+    #     send_message(recipient_id, "User ID " + str(recipient_id) + " is found")
+
+    if users[recipient_id]["state"] == "None":
         if message == "start quiz":
-            state = "quiz"
-            send_message(recipient_id, questions[q_index])
+            users[recipient_id]["state"] = "start quiz"
+            # print(str(users))
+            send_message(recipient_id, questions[users[recipient_id]["q_index"]])
         else:
             send_message(recipient_id, "Send \"start quiz\" to start quiz")
-    elif state == "quiz":
+
+    elif users[recipient_id]["state"] == "start quiz":
         if "end quiz" in message:
-            state = "done quiz"
-            q_index = 0
+            users[recipient_id]["state"] = "done quiz"
             send_message(recipient_id, "Your quiz has been terminated. Send \"Get Result\" to see your result.")
         else:
-            if message == answers[q_index]:
-                send_message(recipient_id, correct_response())
+            if answers[users[recipient_id]["q_index"]] in message:
+                send_message(recipient_id, correct_response(recipient_id))
             else:
-                send_message(recipient_id, "You response is " + message + ". The correct answer is " + answers[q_index])
+                send_message(recipient_id, "You response is " + message + ". The correct answer is " + answers[
+                    users[recipient_id]["q_index"]])
 
-            shuffle_index.pop(0)
-            q_index = shuffle_index[0]
+            users[recipient_id]["q_index"] += 1
 
-
-            if q_index > len(questions) - 1:
-                state = "done quiz"
-                q_index = 0
+            if users[recipient_id]["q_index"] > len(questions) - 1:
+                users[recipient_id]["state"] = "done quiz"
+                users[recipient_id]["q_index"] = 0
                 send_message(recipient_id, "You've reached the end of the quiz.")
                 send_message(recipient_id, "Send \"Get Result\" to see your result.")
             else:
-                send_message(recipient_id, questions[q_index])
+                send_message(recipient_id, questions[users[recipient_id]["q_index"]])
 
-    elif state == "done quiz" and message == "get result":
-        send_message(recipient_id, "Your got " + str(correct_count) + "/" + str(len(questions)) + " correct.")
+    elif users[recipient_id]["state"] == "done quiz":
+        if message == "get result":
+            send_message(recipient_id, "You got " + str(users[recipient_id]["correct_count"]) + "/" + str(
+                len(questions)) + " correct.")
+        users[recipient_id]["state"] = "None"
+        users[recipient_id]["q_index"] = 0
+        users[recipient_id]["correct_count"] = 0
 
-
-
-
-
-
+        send_message(recipient_id, "Enter \"start quiz\" to restart")
 
 
 # ======================== Don't mess with the stuff below!!! ========================
@@ -88,38 +110,63 @@ def verify_fb_token(token_sent):
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
+
 def send_message(recipient_id, response):
-    bot.send_text_message(recipient_id, response) # Sends the 'response' parameter to the user
+    bot.send_text_message(recipient_id, response)  # Sends the 'response' parameter to the user
     return "Message sent"
 
+
+def send_attachment(recipient_id, message):
+    for att in message:
+        bot.send_attachment_url(recipient_id, att['type'], att['payload']['url'])
+
+
 def retrieve_id_and_message():
-    print("retrieve function gets called")
     recipient_id = ""
     message = ""
+    message_type = 'None'
     output = request.get_json()  # get whatever message a user sent the bot
     for event in output['entry']:
         messaging = event['messaging']
         for message in messaging:
             if message.get('message'):
                 recipient_id = message['sender']['id']
-                message = message['message'].get('text').lower()
-    return recipient_id, message
+
+                print(message, message_type)
+
+                if message['message'].get('text'):
+                    message_type = 'text'
+                    message = message['message']['text'].lower()
+                # elif message['message'].get('attachments'):
+                #     message_type = 'attachments'
+                #     message = message['message'].get('attachments')
+
+    return recipient_id, message, message_type
+
 
 # This endpoint will receive messages
-@app.route("/webhook/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def receive_message():
     # Handle GET requests
     if request.method == 'GET':
-        print("GET request gets called")
         token_sent = request.args.get("hub.verify_token")  # Facebook requires a verify token when receiving messages
         return verify_fb_token(token_sent)
 
     # Handle POST requests
     else:
-        recipient_id, message = retrieve_id_and_message()
-        run_program(recipient_id, message)
+        recipient_id, message, message_type = retrieve_id_and_message()
+        if recipient_id != "":
+            if message_type == 'text':
+                run_program(recipient_id, message)
+            else:
+                send_message(recipient_id, 'Sorry, I don\'t understand that.')
+            # elif message_type == 'attachments':
+            #     send_attachment(recipient_id, message)
+
         return "Message Processed"
+
 
 # Ensures that the below code is only evaluated when the file is executed, and ignored if the file is imported
 if __name__ == "__main__":
-    app.run() ## Runs application
+    app.run()  ## Runs application
+on
